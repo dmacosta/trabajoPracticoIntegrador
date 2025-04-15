ac5a94f1d278aae0362ddd2bf78ee9c3df7d9f8e
daimacosta <acostadaianam@gmail.com>
2025-04-14 22:34:24 -0300

    primer punto resuelto

---
+++ tp-integrador.js
@@ -0,0 +1,15 @@
+//ESTRUCTURA DEL CÓDIGO
+/* TRABAJO PRÁCTICO INTEGRADOR - SISTEMA DE GESTIÓN DE BIBLIOTECA */
+
+/* PUNTO 1:1. Estructura de Datos
+Crear un array de objetos llamado libros que contenga al menos 10 libros. Cada libro debe tener las siguientes propiedades: 
+id (número)
+título (string), 
+autor (string), 
+año (número), 
+género (string), 
+disponible (booleano).
+
+ */
+
+git add 
